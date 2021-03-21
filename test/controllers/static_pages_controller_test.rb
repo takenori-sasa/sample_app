ce07003a4646337df5263618b3c75f7bc262ba8b
sasa_takenori <yoshida.tkw@gmail.com>
2021-03-21 10:59:11 +0000

    Add contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -26,6 +26,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
