6517bb2b107fbe3cfd9cf43b5b38f3240b43227c
takenori-sasa <yoshida.tkw@gmail.com>
2021-03-23 22:04:26 +0000

    finish 8.1

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
-class SessionsControllerTest < ActionDispatch::IntegrationTest
+class UsersLoginTest < ActionDispatch::IntegrationTest
 
-  test "should get new" do
+  test "login with invalid information" do
     get login_path
-    assert_response :success
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
   end
 end
