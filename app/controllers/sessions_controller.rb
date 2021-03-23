d4501bc75d88ec2764d50fc7a4969c7067d45c57
takenori-sasa <yoshida.tkw@gmail.com>
2021-03-23 22:41:44 +0000

    finish 8.2.4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+
+
 class SessionsController < ApplicationController
 
   def new
@@ -5,7 +7,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
@@ -15,5 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
