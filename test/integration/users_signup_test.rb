2ba671a34ce84163255432710e32c405bf433aaa
sasa_takenori <yoshida.tkw@gmail.com>
2021-03-23 01:38:34 +0000

    finish 7.4

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,7 +17,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert'
     assert_select 'div.field_with_errors'
   end
-    test "valid signup information" do
+  test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
@@ -27,5 +27,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert_not flash.blank?
   end
 end
