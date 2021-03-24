e59048cc8f1500ec36ae3168cd3ab1481984e8fe
takenori-sasa <yoshida.tkw@gmail.com>
2021-03-24 16:01:50 +0000

    finish 7.1.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
+                                   :password_confirmation)
     end
 end
