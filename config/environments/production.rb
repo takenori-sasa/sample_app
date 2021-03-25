a5cae65d3936bfb12c575746dfaaec1cb80953ba
takenori-sasa <yoshida.tkw@gmail.com>
2021-03-25 21:25:29 +0000

    finish 12.2.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'shrouded-dawn-20476.herokuapp.com/ i'
+  host = 'shrouded-dawn-20476.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
