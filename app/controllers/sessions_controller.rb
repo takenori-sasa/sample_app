a1b0c38589c7a8353e0c4e2131f8c377c715537a
takenori-sasa <yoshida.tkw@gmail.com>
2021-03-25 02:06:52 +0000

    finish 10.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,20 @@
 class SessionsController < ApplicationController
 
   def new
+    
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
+    @user = User.find_by(email: params[:session][:email].downcase)#この@が消えると"login with remembering"test死ぬ
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
+    
   end
 
   def destroy
