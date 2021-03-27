4c1765974e9a63f055fad67b6413cb68b3daf299
takenori-sasa <yoshida.tkw@gmail.com>
2021-03-27 01:54:18 +0000

    finish 14.2.2

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -19,5 +19,14 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    assert_select 'strong#following.stat', "#{@user.following.count}"
+    assert_select 'strong#followers.stat', "#{@user.followers.count}"
   end
+  test "correct stat" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'strong#following.stat', "#{@user.following.count}"
+    assert_select 'strong#followers.stat', "#{@user.followers.count}"
+  end
+  
 end
